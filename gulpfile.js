d16a2aceb6ba52c525111572116d3e49769cf5e3
b-aybulat <aibulatb24@gmail.com>
2021-05-09 15:27:35 +0300

    temp

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,13 @@ let { src, dest } = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     group_media = require('gulp-group-css-media-queries'),
     clean_css = require('gulp-clean-css'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify-es').default,
+    imagemin = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webphtml = require('gulp-webp-html'),
+    webpcss = require('gulp-webp-css'),
+    svgSprite = require('gulp-svg-sprite');
 
 function browserSync(params){
     browsersync.init({
@@ -51,6 +57,7 @@ function browserSync(params){
 function html(){
     return src(path.src.html)
         .pipe(fileinclude())
+        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -71,6 +78,7 @@ function css(){
                 cascade: true
             })
         )
+        .pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -86,22 +94,67 @@ function js(){
     return src(path.src.js)
         .pipe(fileinclude())
         .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
         .pipe(browsersync.stream())
 }
+
+function images(){
+    return src(path.src.img)
+        .pipe(
+            webp({
+                quality: 70
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(
+            imagemin({
+                progressive: true,
+                svgoPlugins: [{ removeViewBox: false }],
+                interlaced: true,
+                optimizationLevel: 3
+            })
+        )  
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
+
+gulp.task('svgSprite', function(){
+    return gulp.src([source_folder + 'iconsprite/*.svg'])
+        .pipe(svgSprite({
+            mode:{
+                stack:{
+                    sprite: "..icons/icons.svg",
+                }
+            }
+        }
+        ))
+        .pipe(dest(path.build.img))
+})
 
 function watchFiles(params){
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images)
 }
 
 function clean(params){
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
